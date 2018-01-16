29cb97b57d1d7dfb9a21b15a3242c68cca932289
Samuel Haugland <samhaug@umich.edu>
2018-01-16 11:32:59 -0500

    commit

--- moveout_correction.py
+++ moveout_correction.py
@@ -7,7 +7,7 @@ File Name : moveout_correction.py
 Purpose : Apply moveout correction to deconvolved data using lookup table.
           lookup table made make_lookup.py
 Creation Date : 15-01-2018
-Last Modified : Mon 15 Jan 2018 05:11:56 PM EST
+Last Modified : Tue 16 Jan 2018 11:32:56 AM EST
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -22,7 +22,6 @@ import argparse
 from scipy.interpolate import interp1d
 
 def main():
-
     parser = argparse.ArgumentParser(description='Clip/write reverb intervals')
     parser.add_argument('-l','--lkup_table', metavar='H5_FILE',type=str,
                         help='h5 lookup table')
@@ -30,22 +29,28 @@ def main():
                         help='h5 file of deconvolved reverberations')
     args = parser.parse_args()
 
-    mvout = h5py.File('mvout.h5','w')
+    mvout = h5py.File('mvout_corrected.h5','w')
     d = h5py.File(args.deconvolve,'r')
     l = h5py.File(args.lkup_table,'r')
 
     for keys in d:
         mvout.create_group(keys)
         gcarc = round(d[keys]['coords'][0])
+        mvout.create_dataset(keys+'/coords',data=d[keys]['coords'][...])
         mapping = l[str(gcarc)][...]
         data = d[keys]['sScSScS'][...]
-        data = np.roll(data,-1*np.argmax(np.abs(data))
-        f = interp1d(mapping[1,:],data)
+        data = np.roll(data,-1*np.argmax(np.abs(data)))
+
+        f = interp1d(mapping[1,0:len(data)],data)
         abs_depth = np.linspace(mapping[1,0],
-                                mapping[1,-1],
-                                num=int(2*mapping[1,-1]))
+                                mapping[1,0:len(data)].max(),
+                                num=int(2*mapping[1,len(data)]))
         mv_data = f(abs_depth)
-        mvout.create_dataset(keys+'/sScSScS',data=mv_data)
+
+        mvout.create_dataset(keys+'/sScSScS',data=np.vstack((abs_depth,mv_data)))
+    mvout.close()
+    d.close()
+    l.close()
 
 main()
 
