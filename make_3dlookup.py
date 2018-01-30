fd59a604d7f2d816dbd47fc740c6c8f2b1c1d12e
Samuel Haugland <samhaug@umich.edu>
2018-01-30 13:19:03 -0500

    commit

--- make_3dlookup.py
+++ make_3dlookup.py
@@ -6,7 +6,7 @@
 File Name : make_3dlookup.py
 Purpose : Make h5 lookup table of reverberation traveltimes for 3d model
 Creation Date : 20-12-2017
-Last Modified : Mon 29 Jan 2018 06:37:48 PM EST
+Last Modified : Tue 30 Jan 2018 01:19:00 PM EST
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -17,6 +17,7 @@ from matplotlib import pyplot as plt
 from obspy.taup import TauPyModel
 from subprocess import call
 import h5py
+from netCDF4 import Dataset
 from scipy.interpolate import interp1d
 from scipy.interpolate import RegularGridInterpolator
 import re
@@ -30,15 +31,16 @@ def main():
                        description='make lookup table for event in 3d model')
     parser.add_argument('-f','--fname', metavar='H5_FILE',type=str,
                         help='h5 stream object')
-    parser.add_argument('-m','--mod', metavar='H5_FILE',type=str,
-                        help='h5 3d model')
     args = parser.parse_args()
-
-    int_3d = interp_3d(args.mod)
+    int_3d = interp_netcdf_3d()
     st = obspy.read(args.fname)
 
-    f = h5py.File(args.fname,'r')
-    h5f = h5py.File('3dmvt_lkup.h5','w')
+    try:
+        h5f = h5py.File('3dmvt_lkup.h5','w',driver='core')
+    except IOError:
+        call('rm 3dmvt_lkup.h5',shell=True)
+        h5f = h5py.File('3dmvt_lkup.h5','w',driver='core')
+
     evdp = st[0].stats.evdp
     phase_lists = [['sScS','sSvXSScS'],
                   ['sScSScS','sSvXSScSScS'],
@@ -49,8 +51,25 @@ def main():
     make_lookup(phase_lists,h5f,st,evdp,int_3d)
     h5f.close()
 
+def interp_netcdf_3d():
+    dataset = Dataset('/home/samhaug/work1/China_ScSn_code/3D_model/3D2016-09Sv-depth.nc')
+    lat = dataset.variables['latitude'][::-1]
+    lon = dataset.variables['longitude'][:]
+    h = dataset.variables['depth'][:]
+    h = np.hstack((0,h))
+    dvs = dataset.variables['dvs'][:].data
+    top = np.zeros((1,dvs.shape[1],dvs.shape[2]))
+    dvs = np.vstack((top,dvs))
+    for ii in range(15,len(dvs)):
+        dvs[ii] = np.zeros(dvs[ii].shape)
+    #We use 1-(dvs/100.) to adjust ttimes
+    plt.imshow(dvs[2],aspect='auto',extent=[lon[0],lon[-1],lat[0],lat[-1]])
+    plt.show()
+    int_3d = RegularGridInterpolator((h,lat,lon),dvs)
+    return int_3d
+
 def interp_3d(mod):
-    mod = h5py.File(mod,'r')
+    mod = h5py.File(mod,'r',driver='core')
     dvs = mod['dvs'][:]
     top = np.zeros((1,dvs.shape[1],dvs.shape[2]))
     dvs = np.vstack((top,dvs))
@@ -60,6 +79,8 @@ def interp_3d(mod):
     lat = lat[::-1]
     lon = mod['lon'][:]
     int_3d = RegularGridInterpolator((h,lat,lon),dvs)
+    for ii in range(15,len(dvs)):
+        dvs[ii] = np.zeros(dvs[ii].shape)
     mod.close()
     return int_3d
 
@@ -67,80 +88,100 @@ def make_lookup(phase_lists,h5f,st,evdp,int_3d):
     cdp = np.arange(50,1800,10)
     for phase_list in phase_lists:
         for tr in st:
+            name = tr.stats.network+tr.stats.station+tr.stats.location
             print 'Computing '+phase_list[0]
 
             master_time = []
+            master_time_3d = []
             master_depth = []
 
             if phase_list[0][0].startswith('s'):
                 for ii in cdp:
-                    time_list,depth_list,gcarc_list = top_depth_times(evdp,
+                    time_list,time_list_3d,depth_list = top_depth_times(evdp,
                                                       ii,phase_list,tr,int_3d)
                     master_time.append(time_list)
+                    master_time_3d.append(time_list_3d)
                     master_depth.append(depth_list)
             elif phase_list[0][0].startswith('S'):
                 for ii in cdp:
-                    time_list,depth_list,gcarc_list = bot_depth_times(evdp,
+                    time_list,time_list_3d,depth_list = bot_depth_times(evdp,
                                                       ii,phase_list)
                     master_time.append(time_list)
+                    master_time_3d.append(time_list_3d)
                     master_depth.append(depth_list)
 
             master_time = np.array(master_time)
             master_time = np.vstack((np.zeros(master_time.shape[1]),
                                      master_time))
+            master_time_3d = np.array(master_time_3d)
+            master_time_3d = np.vstack((np.zeros(master_time_3d.shape[1]),
+                                     master_time_3d))
             master_depth = np.array(master_depth)
             master_depth = np.vstack((np.zeros(master_depth.shape[1]),
                                      master_depth))
 
-            for idx,ii in enumerate(gcarc_list):
-                f = interp1d(master_time[:,idx],master_depth[:,idx])
-                tnew = np.arange(master_time[0,idx],master_time[-1,idx],0.1)
-                dnew = f(tnew)
-                h5f.create_dataset(phase_list[0]+'/'+str(ii),
-                                   data=np.vstack((tnew,dnew)))
+            h5f.create_dataset(name+'/time',data=master_time)
+            h5f.create_dataset(name+'/time_3d',data=master_time_3d)
+            h5f.create_dataset(name+'/depth',data=master_depth)
 
 def top_depth_times(evdp,cdp,phase_list_in,tr,int_3d):
     mod = TauPyModel(model='prem'+str(int(cdp)))
     phase_list = phase_list_in[:]
     phase_list[1] = phase_list[1].replace('X',str(cdp))
-    time_list = []
     depth_list = []
+    time_list = []
+    time_list_3d = []
+
 
     arr = mod.get_ray_paths_geo(source_depth_in_km=evdp,
-                               source_latitude_in_deg=tr.stats.evla,
-                               source_longtude_in_deg=tr.stats.evlo,
-                               receiver_latitude_in_deg=tr.stats.stla,
-                               receiver_longitude_in_deg=tr.stats.stlo,
-                               phase_list=phase_list)
+                                source_latitude_in_deg=tr.stats.evla,
+                                source_longitude_in_deg=tr.stats.evlo,
+                                receiver_latitude_in_deg=tr.stats.stla,
+                                receiver_longitude_in_deg=tr.stats.stlo,
+                                phase_list=phase_list)
 
     pure_depth = float(re.findall('\d+',arr[1].purist_name)[0])
+    depth_list.append(pure_depth)
     time = arr[1].time-arr[0].time
     time_list.append(time)
-    paths = np.array([list((i[3],i[4],i[5])) for i in arr.path])
-    depth_list.append(pure_depth)
+    main_path = np.array([list((i[3],i[4],i[5])) for i in arr[0].path])
+    rev_path = np.array([list((i[3],i[4],i[5])) for i in arr[1].path])
+    main_trace = 1-(int_3d(main_path)[1::]*0.01)
+    rev_trace = 1-(int_3d(rev_path)[1::]*0.01)
+    main_dt = np.diff(np.array([i[1] for i in arr[0].path]))
+    rev_dt = np.diff(np.array([i[1] for i in arr[1].path]))
+    time_list_3d.append(np.sum(rev_dt*rev_trace)-np.sum(main_dt*main_trace))
 
-    r = np.array([list(i) for i in arr[0].path])
+    return time_list,time_list_3d,depth_list
 
-    return time_list,depth_list,gcarc_list
-
-def bot_depth_times(evdp,cdp,phase_list_in):
+def bot_depth_times(evdp,cdp,phase_list_in,tr,int_3d):
     mod = TauPyModel(model='prem'+str(int(cdp)))
     phase_list = phase_list_in[:]
     phase_list[1] = phase_list[1].replace('X',str(cdp))
-    time_list = []
     depth_list = []
-    gcarc_list = np.linspace(0,90,num=91)
+    time_list = []
+    time_list_3d = []
 
-    for ii in gcarc_list:
-        arr = mod.get_ray_paths(source_depth_in_km=evdp,
-                                   distance_in_degree=ii,
-                                   phase_list=phase_list)
-        pure_depth = float(re.findall('\d+',arr[0].purist_name)[0])
-        time = arr[1].time-arr[0].time
-        time_list.append(time)
-        depth_list.append(pure_depth)
+    arr = mod.get_ray_paths_geo(source_depth_in_km=evdp,
+                                source_latitude_in_deg=tr.stats.evla,
+                                source_longitude_in_deg=tr.stats.evlo,
+                                receiver_latitude_in_deg=tr.stats.stla,
+                                receiver_longitude_in_deg=tr.stats.stlo,
+                                phase_list=phase_list)
 
-    return time_list,depth_list,gcarc_list
+    pure_depth = float(re.findall('\d+',arr[1].purist_name)[0])
+    depth_list.append(pure_depth)
+    time = arr[1].time-arr[0].time
+    time_list.append(time)
+    main_path = np.array([list((i[3],i[4],i[5])) for i in arr[1].path])
+    rev_path = np.array([list((i[3],i[4],i[5])) for i in arr[0].path])
+    main_trace = 1-(int_3d(main_path)[1::]*0.01)
+    rev_trace = 1-(int_3d(rev_path)[1::]*0.01)
+    main_dt = np.diff(np.array([i[1] for i in arr[1].path]))
+    rev_dt = np.diff(np.array([i[1] for i in arr[0].path]))
+    time_list_3d.append(np.sum(main_dt*main_trace-np.sum(rev_dt*rev_trace)))
+
+    return time_list,time_list_3d,depth_list
 
 main()
 
