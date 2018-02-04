830e7788879419b8af3406962be9ceeb8b0ae4c7
Samuel Haugland <samhaug@umich.edu>
2018-02-04 14:54:09 -0500

    commit

--- moveout_3dcorrection.py
+++ moveout_3dcorrection.py
@@ -7,7 +7,7 @@ File Name : moveout_correction.py
 Purpose : Apply moveout correction to deconvolved data using lookup table.
           lookup table made make_lookup.py
 Creation Date : 15-01-2018
-Last Modified : Sun 04 Feb 2018 02:23:27 PM EST
+Last Modified : Sun 04 Feb 2018 02:54:04 PM EST
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -29,9 +29,9 @@ def main():
                         help='h5 file of deconvolved reverberations')
     args = parser.parse_args()
 
-    mvout = h5py.File('mvt_correct.h5','w')
-    d = h5py.File(args.deconvolve,'r')
-    l = h5py.File(args.lkup_table,'r')
+    mvout = h5py.File('3dmvt_correct.h5','w',driver='core')
+    d = h5py.File(args.deconvolve,'r',driver='core')
+    l = h5py.File(args.lkup_table,'r',driver='core')
 
     for dkeys in d:
         gcarc = round(d[dkeys]['coords'][0])
@@ -40,17 +40,22 @@ def main():
             for lkeys in d[dkeys]:
                 if not lkeys.startswith('c'):
                     print dkeys,lkeys
-                    mapping = l[dkeys][lkeys][:]
-                    data = d[dkeys][lkeys][:]
-                    data = np.roll(data,-1*np.argmax(np.abs(data)))
-                    f = interp1d(mapping[1,0:len(data)],data)
-                    abs_depth = np.linspace(mapping[1,0],
-                                            mapping[1,0:len(data)].max(),
-                                            num=int(2*mapping[1,len(data)]))
-                    mv_data = f(abs_depth)
-                    mv_data *= 1./np.abs(mv_data).max()
-                    mvout.create_dataset(dkeys+'/'+lkeys,
-                                         data=np.vstack((abs_depth,mv_data)))
+                    mapping_lkup = l[dkeys][lkeys]
+                    for ckeys in mapping_lkup:
+                        if ckeys.startswith('S') or ckeys.startswith('s'):
+                            d_map = mapping_lkup[ckeys]['depth']
+                            t_map = mapping_lkup[ckeys]['3d_time']
+                            data = d[dkeys][lkeys][:]
+                            data = np.roll(data,-1*np.argmax(np.abs(data)))
+
+                            f = interp1d(mapping[1,0:len(data)],data)
+                            abs_depth = np.linspace(mapping[1,0],
+                                        mapping[1,0:len(data)].max(),
+                                        num=int(2*mapping[1,len(data)]))
+                            mv_data = f(abs_depth)
+                            mv_data *= 1./np.abs(mv_data).max()
+                            mvout.create_dataset(dkeys+'/'+lkeys,
+                                        data=np.vstack((abs_depth,mv_data)))
         except KeyError:
             continue
     mvout.close()
