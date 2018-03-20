f7c5f2f45d664f28782478fa3a6aafc3ee88ad88
Samuel Haugland <samhaug@umich.edu>
2018-03-20 11:00:01 -0400

    commit

--- radon_transform.py
+++ radon_transform.py
@@ -6,7 +6,7 @@
 File Name : radon_transform.py
 Purpose : apply radon transform to trace.
 Creation Date : 19-03-2018
-Last Modified : Tue 20 Mar 2018 10:33:20 AM EDT
+Last Modified : Tue 20 Mar 2018 11:00:00 AM EDT
 Created By : Samuel M. Haugland
 
 ==============================================================================
@@ -50,6 +50,8 @@ def main():
         stc[idx].data = d[idx]
     #seispy.plot.simple_h5_section(stc)
     stc.write('st_T_radon.h5',format='H5')
+    ax = gca()
+    cc = clicker_class(ax)
 
 def prepare_input(st):
     p = np.arange(-8.0,8.1,0.1)
@@ -83,7 +85,76 @@ def block_stream(st):
         st[idx].data = tr.data[0:40000]
     return st
 
+class clicker_class(object):
+    def __init__(self, ax, pix_err=1):
+        self.canvas = ax.get_figure().canvas
+        self.cid = None
+        self.pt_lst = []
+        self.pt_plot = ax.plot([], [], marker='o',
+                               linestyle='none', zorder=5)[0]
+        self.pix_err = pix_err
+        self.connect_sf()
+
+    def set_visible(self, visible):
+        '''sets if the curves are visible '''
+        self.pt_plot.set_visible(visible)
+
+    def clear(self):
+        '''Clears the points'''
+        self.pt_lst = []
+        self.redraw()
+
+    def connect_sf(self):
+        if self.cid is None:
+            self.cid = self.canvas.mpl_connect('button_press_event',
+                                               self.click_event)
+
+    def disconnect_sf(self):
+        if self.cid is not None:
+            self.canvas.mpl_disconnect(self.cid)
+            self.cid = None
+
+    def click_event(self, event):
+        ''' Extracts locations from the user'''
+        if event.key == 'shift':
+            self.pt_lst = []
+            return
+        if event.xdata is None or event.ydata is None:
+            return
+        if event.button == 1:
+            self.pt_lst.append((event.xdata, event.ydata))
+        elif event.button == 3:
+            self.remove_pt((event.xdata, event.ydata))
+
+        self.redraw()
+
+    def remove_pt(self, loc):
+        if len(self.pt_lst) > 0:
+            self.pt_lst.pop(np.argmin(map(lambda x:
+                                          np.sqrt((x[0] - loc[0]) ** 2 +
+                                                  (x[1] - loc[1]) ** 2),
+                                          self.pt_lst)))
+
+    def redraw(self):
+        if len(self.pt_lst) > 0:
+            x, y = zip(*self.pt_lst)
+        else:
+            x, y = [], []
+        self.pt_plot.set_xdata(x)
+        self.pt_plot.set_ydata(y)
+
+        self.canvas.draw()
+
+    def return_points(self):
+        '''Returns the clicked points in the format the rest of the
+        code expects'''
+        return np.vstack(self.pt_lst).T
+
+
 main()
+
+
+
 
 
 
