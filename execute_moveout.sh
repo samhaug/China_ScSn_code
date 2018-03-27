796047836e9f702c7ee37e60157333920b79730a
Samuel Haugland <samhaug@umich.edu>
2018-03-27 15:29:35 -0400

    commit

--- execute_moveout.sh
+++ execute_moveout.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-python extract_data_reverb.py -s sts_T_radon.h5 -d st_T_radon.h5
+s_bin=/home/samhaug/work1/China_ScSn_code/
 
-python deconvolve.py -f data_reverb.h5 -d data_deconvolve.h5
-python deconvolve.py -f synth_reverb.h5 -d synth_deconvolve.h5
+echo extract_data_reverb.py
+python $s_bin'extract_data_reverb.py' -s sts_T_radon.h5 -d st_T_radon.h5
 
-python make_3dlookup.py -f synth_deconvolve.h5
-python make_3dreflection_lookup.py -f synth_deconvolve.h5
+echo deconvolve data
+python $s_bin'/deconvolve.py' -f data_reverb.h5 -d data_deconvolve.h5
+echo deconvolve synth
+python $s_bin'/deconvolve.py' -f synth_reverb.h5 -d synth_deconvolve.h5
 
-python grid_3dsum.py -d synth_deconvolve.h5 -l 3dmvt_lkup.h5 -r 3dreflection_points.h5 -g synth_grid.h5
+echo make_3dlookup.py
+python $s_bin'/make_3dlookup.py' -f synth_deconvolve.h5
+echo make_3dreflection_lookup.py
+python $s_bin'/make_3dreflection_lookup.py' -f synth_deconvolve.h5
 
-python grid_3dsum.py -d data_deconvolve.h5 -l 3dmvt_lkup.h5 -r 3dreflection_points.h5 -g data_grid.h5
+echo synth_grid
+python $s_bin'/grid_3dsum.py' -d synth_deconvolve.h5 -l 3dmvt_lkup.h5 -r 3dreflection_points.h5 -g synth_grid.h5
+
+echo data grid
+python $s_bin'/grid_3dsum.py' -d data_deconvolve.h5 -l 3dmvt_lkup.h5 -r 3dreflection_points.h5 -g data_grid.h5
 
