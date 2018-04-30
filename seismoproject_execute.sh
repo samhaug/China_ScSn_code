a9ad1193b358805a2651243285693b525fcc28ac
Samuel McRae Haugland <samhaug@umich.edu>
2018-04-30 16:28:02 -0400

    commit

--- seismoproject_execute.sh
+++ seismoproject_execute.sh
@@ -1,11 +1,10 @@
 #/bin/bash 
-                                                                                
 #--read_catalog $read_catalog                                                   
 echo "# Sample PBS for parallel jobs'" > execute_submit.pbs 
 echo "#PBS -A seismoproject_fluxod"    >>execute_submit.pbs
 echo "#PBS -q fluxod"                  >>execute_submit.pbs 
 echo "#PBS -l qos=flux"                >>execute_submit.pbs     
-echo "#PBS -N h5_submit"               >>execute_submit.pbs 
+echo "#PBS -N execute_submit"          >>execute_submit.pbs 
 echo "#PBS -l nodes=1:ppn=1,pmem=8gb,walltime=48:00:00">>execute_submit.pbs    
 echo "#PBS -d ."                       >>execute_submit.pbs  
 echo "#PBS -m n"                       >>execute_submit.pbs   
@@ -13,5 +12,5 @@ echo "#PBS -V"                         >>execute_submit.pbs
 echo "#PBS -o execute.out"             >>execute_submit.pbs    
 echo "#PBS -e execute.err"             >>execute_submit.pbs  
 echo "ulimit -s unlimited "            >>execute_submit.pbs  
-echo "./execute_data_moveout.sh"       >> execute_submit.pbs
+echo "./execute_data_moveout.sh"       >>execute_submit.pbs
 qsub execute_submit.pbs                                                      
